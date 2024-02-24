b0be63c3f5f32e415d7533e0d29be73e6c43a83a
developerSajib88 <developersajib88@gmail.com>
2024-02-24 16:08:05 +0600

    folder structure create according with mvvm architecture

---
+++ lib/product/model/product_res_model.dart
@@ -0,0 +1,133 @@
+import 'dart:convert';
+
+class ProductRes {
+  final List<Product>? products;
+  final int? total;
+  final int? skip;
+  final int? limit;
+
+  ProductRes({
+    this.products,
+    this.total,
+    this.skip,
+    this.limit,
+  });
+
+  ProductRes copyWith({
+    List<Product>? products,
+    int? total,
+    int? skip,
+    int? limit,
+  }) =>
+      ProductRes(
+        products: products ?? this.products,
+        total: total ?? this.total,
+        skip: skip ?? this.skip,
+        limit: limit ?? this.limit,
+      );
+
+  factory ProductRes.fromRawJson(String str) => ProductRes.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory ProductRes.fromJson(Map<String, dynamic> json) => ProductRes(
+    products: json["products"] == null ? [] : List<Product>.from(json["products"]!.map((x) => Product.fromJson(x))),
+    total: json["total"],
+    skip: json["skip"],
+    limit: json["limit"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "products": products == null ? [] : List<dynamic>.from(products!.map((x) => x.toJson())),
+    "total": total,
+    "skip": skip,
+    "limit": limit,
+  };
+}
+
+class Product {
+  final int? id;
+  final String? title;
+  final String? description;
+  final int? price;
+  final double? discountPercentage;
+  final double? rating;
+  final int? stock;
+  final String? brand;
+  final String? category;
+  final String? thumbnail;
+  final List<String>? images;
+
+  Product({
+    this.id,
+    this.title,
+    this.description,
+    this.price,
+    this.discountPercentage,
+    this.rating,
+    this.stock,
+    this.brand,
+    this.category,
+    this.thumbnail,
+    this.images,
+  });
+
+  Product copyWith({
+    int? id,
+    String? title,
+    String? description,
+    int? price,
+    double? discountPercentage,
+    double? rating,
+    int? stock,
+    String? brand,
+    String? category,
+    String? thumbnail,
+    List<String>? images,
+  }) =>
+      Product(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        description: description ?? this.description,
+        price: price ?? this.price,
+        discountPercentage: discountPercentage ?? this.discountPercentage,
+        rating: rating ?? this.rating,
+        stock: stock ?? this.stock,
+        brand: brand ?? this.brand,
+        category: category ?? this.category,
+        thumbnail: thumbnail ?? this.thumbnail,
+        images: images ?? this.images,
+      );
+
+  factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+    id: json["id"],
+    title: json["title"],
+    description: json["description"],
+    price: json["price"],
+    discountPercentage: json["discountPercentage"]?.toDouble(),
+    rating: json["rating"]?.toDouble(),
+    stock: json["stock"],
+    brand: json["brand"],
+    category: json["category"],
+    thumbnail: json["thumbnail"],
+    images: json["images"] == null ? [] : List<String>.from(json["images"]!.map((x) => x)),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "description": description,
+    "price": price,
+    "discountPercentage": discountPercentage,
+    "rating": rating,
+    "stock": stock,
+    "brand": brand,
+    "category": category,
+    "thumbnail": thumbnail,
+    "images": images == null ? [] : List<dynamic>.from(images!.map((x) => x)),
+  };
+}
